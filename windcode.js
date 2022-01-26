720e45bf2dfff43da0cad9453f47501dd7d17817
Burak <51405817+oyunusta@users.noreply.github.com>
2022-01-26 03:55:22 +0300

    Create windcode.js

---
+++ windcode.js
@@ -0,0 +1,106 @@
+const { Client, MessageActionRow, MessageSelectMenu, MessageButton, MessageEmbed, Collection } = require('discord.js')
+const { token, prefix  } = require('./ayarlar/config')
+const client = new Client({ intents: 32767 })
+const {Manager} = require('discord-autorole-badges')
+const chalk = require('chalk')
+const fs = require('fs')
+const config = require(`./ayarlar/roller`)
+client.on('ready', async () => {
+    client.user.setPresence({ activities:[{ name: "</> Levian Code" }], status: "idle" })
+})
+
+client.on('messageCreate', async message => {
+    try{
+        let client = message.client
+        if (message.author.bot) return
+        if (message.channel.type == "DM") return
+        if (!message.content.startsWith(prefix)) return
+        let command = message.content.split(' ')[0].slice(prefix.length)
+        let params = message.content.split(' ').slice(1)
+        let cmd
+        if (client.commands.has(command)) {
+            cmd = client.commands.get(command)
+        } else if (client.aliases.has(command)) {
+            cmd = client.commands.get(client.aliases.get(command))
+        }
+        if (cmd) {
+            cmd.run(client, message, params)
+        }
+    }catch(e){
+        message.reply({ embeds: [
+            new MessageEmbed()
+            .setDescription(`**Beklenmedik bir hatayla karşılaştık!**`)
+        ] })
+        console.log(e)
+    }
+})
+
+const log = message => {
+    console.log(`${message}`)
+}
+
+client.aliases = new Collection()
+client.commands = new Collection()
+fs.readdir('./commands/', (err, files) => {
+    if(!files) return console.log(chalk.bold.red(`Komutlar klasörü yok!`))
+    if (err) console.error(err)
+    log(chalk.bold.blue(`${files.length} komut yüklenecek.`))
+    files.forEach(f => {
+        let props = require(`./commands/${f}`)
+        log(chalk.bold.gray(`Yüklenen komut: ${props.name}.`))
+        client.commands.set(props.name, props)
+        props.aliases.forEach(alias => {
+            client.aliases.set(alias, props.name)
+        })
+    })
+})
+
+
+client.login(token).then(() => console.log(chalk.bold.green("LC - Giriş başarılı"))).catch(e => {
+    console.log(e)
+    console.log(chalk.bold.red("LC - Giriş başarısız"))
+    process.exit(1)
+})
+
+client.on("guildMemberAdd", (member) => {
+
+message.channel.send(`
+👋 **Hey!** selam ${member.user}, **WindCode** sunucumuza hoşgeldin.
+
+🌎 Seni aramızda görmek çok güzel.
+
+📚 Sunucuda sohbete başlamadan önce <#935635705907003402> kanalındaki kuralları okuyun. 
+`)
+
+const row = new MessageActionRow() 
+.addComponents(
+  new MessageButton() 
+
+.setStyle('PRIMARY')
+
+.setLabel('Karşıla!')
+.setEmoji('935698971240849420')
+.setCustomId(`hello`) 
+  )
+client.channels.cache.get('935638992521674792').send({embeds: [embed], components: [row]})
+
+}) 
+
+let manager = new Manager(client, {
+    DISCORD_EMPLOYEE: config.discordstaff,
+    PARTNERED_SERVER_OWNER: config.discordpartner,
+    HYPESQUAD_EVENTS: config.hypesquadevents,
+    BUGHUNTER_LEVEL_1: config.bughunterlevel1,
+    HOUSE_BRAVERY: config.hypesquadbravery,
+    HOUSE_BRILLIANCE: config.hypesquadbrilliance,
+    HOUSE_BALANCE: config.hypesquadbalance,
+    EARLY_SUPPORTER: config.earlysupporter,
+    BUGHUNTER_LEVEL_2: config.bughunterlevel2,
+    EARLY_VERIFIED_BOT_DEVELOPER: config.botdeveloper,
+    DISCORD_CERTIFIED_MODERATOR: config.discordmod,
+
+})
+
+client.on("guildMemberAdd", async (member) => {
+    await manager.setRole(member);
+});
